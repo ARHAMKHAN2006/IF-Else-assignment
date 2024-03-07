7ebdf968d3abf90b09b9ec3050a0b94aa96eb555
Muhammad Arham Khan <arhamqadri2006@gmail.com>
2024-03-08 01:11:12 +0500

    js assignment

---
+++ app.js
@@ -0,0 +1,70 @@
+
+// Q 1
+// var num = +prompt("Enter a number and I'll tell you if it is divisible by 3 or not!")
+
+// if (num%3 ==0){
+//     console.log("This number is divisible by 3.")
+// }else{
+//     console.log("Number is not divisible by 3")
+// }
+
+
+
+
+
+
+
+
+// Q 2
+// var num = +prompt("Enter a number and I'll tell you if it is an even or an odd number!")
+
+// if (num%2 ==0){
+//     document.write("This is an even number.")
+// }else{
+//     document.write("This is an odd number.")
+// }
+
+
+
+
+
+
+
+
+
+// Q 3
+// var num = +prompt("Enter your age and I'll determine if you are old enough to visit this site or not!")
+
+// if (num >= 18){
+//     document.write("You are old enough to visit this site.")
+// }else if(num == 0){
+//     document.write("You aren't even born yet.")
+// }else{
+//     document.write("You are not old enough to visit this site.")
+// }
+
+
+
+
+
+// Q 5
+// var password = prompt('Enter your password');
+// var rePassword = prompt('Match your password');
+
+// if (password === rePassword){
+//     document.write('Welcome');
+// }else{
+//     alert('Passwords do not match')
+// }
+
+
+
+// Q 8
+
+
+// var year = +prompt('Enter a calender year in the following format!\nex: 2024');
+// if(year%4 == 0){
+//     alert(year + ' is a leap year.')
+// }else{
+//     alert(year + ' is not a leap year.')
+// }
